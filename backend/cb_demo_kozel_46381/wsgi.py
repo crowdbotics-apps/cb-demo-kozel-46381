346269f3985df388f13b6be48c313028db7be5f4
Crowdbotics <team@crowdbotics.com>
2024-02-15 20:24:53 +0000

    Initial commit

---
+++ backend/cb_demo_kozel_46381/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for cb_demo_kozel_46381 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "cb_demo_kozel_46381.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cb-demo-kozel-46381",
+  url: "https://cb-demo-kozel-46381.botics.co",
+  api: "https://cb-demo-kozel-46381.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
